50c2711adc800a8c937fc0a73f56868466952174
azamfuadi <azam.fuadi@aiesec.net>
2025-11-04 10:30:46 +0900

    adding requirements and finishing the app

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, redirect, url_for, request, render_template, jsonify
 
-from flask import Flask, redirect, url_for, request
+from flaredantic import FlareTunnel, FlareConfig
+from pathlib import Path
+import threading
+
 app = Flask(__name__)
+
+public_url = ''
 
 @app.route('/dashboard/<name>')
 def dashboard(name):
    return 'welcome %s' % name
+
+@app.route('/publiclocalhost')
+def publiclocalhost():
+   global public_url
+   return jsonify({'public_url': public_url})
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
@@ -17,6 +25,21 @@ def login():
    else:
       user = request.args.get('name')
       return render_template('login.html')
+      
+   
+def run_tunnel():
+   port = 8080
+   global public_url
+   cloudflare_config = FlareConfig(
+      port=port, 
+      bin_dir=Path.home()/".my-tunnnels",
+      timeout=60, 
+      verbose=True
+   )
+   with FlareTunnel(cloudflare_config) as tunnel:
+      print(f"http://localhost:{port} available at: {tunnel.tunnel_url}")
+      public_url = tunnel.tunnel_url
+      app.run(host='0.0.0.0', port='5000')
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   threading.Thread(target=run_tunnel).start()
